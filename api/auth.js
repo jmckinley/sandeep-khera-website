0a1c9dcc844c73e0a303d1f9481753e687f32d34
John McKinley <John@greatfallsventures.com>
2026-01-13 19:26:10 -0500

    Update URLs to sandeepkhera.vercel.app

--- api/auth.js
+++ api/auth.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
     return res.status(500).send('GITHUB_CLIENT_ID not configured');
   }
 
-  const redirectUri = `https://fixedwebsite.vercel.app/api/callback`;
+  const redirectUri = `https://sandeepkhera.vercel.app/api/callback`;
   const scope = 'repo,user';
 
   const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`;
